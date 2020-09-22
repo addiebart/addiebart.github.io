3968caa8ad59f437986e036dfb76676fc5cf330f
Eddie (Bart) <70488539+edbart@users.noreply.github.com>
2020-09-22 12:29:20 -0500

    Update ssl-redirect.js

--- docs/stuweb-ajax/ssl-redirect.js
+++ docs/stuweb-ajax/ssl-redirect.js
@@ -1,3 +1,3 @@
 if (document.location.protocol==="http:") {
-  document.location.href=document.location.href.replace("http://"+document.location.host, "https://stuweb.edbart.com");
+  document.location.protocol="https:";
 }
